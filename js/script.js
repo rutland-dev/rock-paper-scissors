471cc4e8f351beaa2f48f2e5f4e9d0231204703e
Joshua Rutland <144169110+rutland-dev@users.noreply.github.com>
2023-10-20 07:39:41 -0400

    Make gameplay basically functional
    Wrote javascript to make the game playable and keep track of score.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Create an array with the choices rock, paper, and scissors.
 
-const choices = ["rock", "paper", "scissors"];
+const choices = ["rock-button", "paper-button", "scissors-button"];
 
 let playerScore = 0;
 let computerScore = 0;
@@ -20,60 +20,107 @@ buttons.forEach((button) => {
     button.addEventListener('click', () => {
         const currentImage = document.querySelector('#result-container img');
         currentImage.remove();
-        const resultContainer = document.querySelector('#result-container');
-        const resultImage = document.createElement('img');
-        resultImage.src = `images/${button.id}.png`;
-        resultContainer.appendChild(resultImage);
+        
+        round(button.id);
     });
 });
 
-function getPlayerChoice() {
-    if (playerSelection === 1) {
-        return "rock";
-    } else if (playerSelection === 2) {
-        return "paper";
-    } else if (playerSelection === 3) {
-        return "scissors";
-    } else {
-        alert("Please make a valid selection");
-        return getPlayerChoice();
-    }
-};
 
 // Write a function that plays a single round of the game. It should take two parameters - playerSelection and computerSelection - and then
 // return a string that declares the winner.
 
-function round() {
+function round(choice) {
     numberOfRounds = numberOfRounds + 1;
-    let playerSelection = getPlayerChoice();
-    console.log(`You chose ${playerSelection}.`);
+    let playerSelection = choice;
     let computerSelection = getComputerChoice();
     console.log(`The computer chose ${computerSelection}.`)
     if (playerSelection === computerSelection) {
+        const resultContainer = document.querySelector('#result-container');
+        const resultImage = document.createElement('img');
+
+        resultContainer.appendChild(resultImage);
+
         numberOfRounds = numberOfRounds - 1;
         console.log("Draw!");
-    } else if (playerSelection === "rock") {
-        if (computerSelection === "paper") {
+    } else if (playerSelection === "rock-button") {
+        if (computerSelection === "paper-button") {
+            const resultContainer = document.querySelector('#result-container');
+            const resultImage = document.createElement('img');
+            
+            resultImage.src = 'images/paper-covers-rock.png';
+            resultContainer.appendChild(resultImage);
+            
             computerScore = computerScore + 1;
+            
+            let computerScoreText = document.querySelector('#computer-score-number');
+            computerScoreText.textContent = computerScore.toString();
+            
             console.log("Paper covers rock. Computer wins!");
         } else {
+            const resultContainer = document.querySelector('#result-container');
+            const resultImage = document.createElement('img');
+            
+            resultImage.src = 'images/rock-breaks-scissors.png';
+            resultContainer.appendChild(resultImage);
+
             playerScore = playerScore + 1;
+            let playerScoreText = document.querySelector('#player-score-number');
+            playerScoreText.textContent = playerScore.toString();
+
             console.log("Rock breaks scissors. You win!");
         }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
+    } else if (playerSelection === "paper-button") {
+        if (computerSelection === "rock-button") {
+            const resultContainer = document.querySelector('#result-container');
+            const resultImage = document.createElement('img');
+            
+            resultImage.src = 'images/paper-covers-rock.png';
+            resultContainer.appendChild(resultImage);
+
             playerScore = playerScore + 1;
+            let playerScoreText = document.querySelector('#player-score-number');
+            playerScoreText.textContent = playerScore.toString();
+
             console.log("Paper covers rock. You win!");
         } else {
+            const resultContainer = document.querySelector('#result-container');
+            const resultImage = document.createElement('img');
+            
+            resultImage.src = 'images/scissors-cut-paper.png';
+            resultContainer.appendChild(resultImage);
+
             computerScore = computerScore + 1;
+            let computerScoreText = document.querySelector('#computer-score-number');
+            computerScoreText.textContent = computerScore.toString();
+
             console.log("Scissors cuts paper. Computer wins!");
         }
     } else {
-        if (computerSelection === "rock") {
+        if (computerSelection === "rock-button") {
+            const resultContainer = document.querySelector('#result-container');
+            const resultImage = document.createElement('img');
+            
+            resultImage.src = 'images/rock-breaks-scissors.png';
+            resultContainer.appendChild(resultImage);
+
             computerScore = computerScore + 1;
+            computerScore = computerScore + 1;
+            let computerScoreText = document.querySelector('#computer-score-number');
+            computerScoreText.textContent = computerScore.toString();
+            
             console.log("Rock breaks scissors. Computer wins!")
         } else {
+            const resultContainer = document.querySelector('#result-container');
+            const resultImage = document.createElement('img');
+            
+            resultImage.src = 'images/scissors-cut-paper.png';
+            resultContainer.appendChild(resultImage);
+
             playerScore = playerScore + 1;
+            playerScore = playerScore + 1;
+            let playerScoreText = document.querySelector('#player-score-number');
+            playerScoreText.textContent = playerScore.toString();
+
             console.log("Scissors cut paper. You win!")
         }
     }
